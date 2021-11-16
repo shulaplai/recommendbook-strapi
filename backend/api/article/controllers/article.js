1dca14d56bd15c96090376236dc231c09910d6ac
Your Name <you@example.com>
2021-11-16 18:21:15 +0800

    change

--- backend/api/article/controllers/article.js
+++ backend/api/article/controllers/article.js
@@ -13,14 +13,4 @@ module.exports = {
     const entity = await strapi.services.article.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.article });
   },
-  async create(ctx) {
-    let entity;
-    if (ctx.is("multipart")) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.restaurant.create(data, { files });
-    } else {
-      entity = await strapi.services.restaurant.create(ctx.request.body);
-    }
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  },
 };
